46ccc04adbbc2d1938232d1b1450193b3133dabd
shzy2012 <shzy2012@gmail.com>
2013-11-24 12:09:08 +0800

    Update GPIO.py

--- GPIO.py
+++ GPIO.py
@@ -1,1 +1,16 @@
 import RPi.GPIO as gpio
+
+gpio.setmode(gpio.BOARD)
+
+gpio.setup(11,gpio.IN)
+gpio.setup(12,gpio.OUT)
+
+input_value = gpio.input(11)
+
+gpio.output(12,gpio.HIGH)
+
+gpio.setmode(gpio.BCM)
+gpio.setup(17,gpio.IN)
+gpio.setup(18,gpio.OUT)
+input_value = gpio.input(17)
+gpio.output(18,gpio.HIGH)
