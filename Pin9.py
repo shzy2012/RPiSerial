c86534a0f1c29f943fea26c156c6397b53e2fb7c
shzy2012 <shzy2012@gmail.com>
2013-11-24 17:17:12 +0800

    Update Pin9.py
    add  7,8,25 pins

--- Pin9.py
+++ Pin9.py
@@ -5,7 +5,7 @@ import sys
 
 # golbal  define
 
-led_port_list = [9,11]
+led_port_list = [7,8,9,11,25]
 
 # define def  -----Begin
 
@@ -20,9 +20,9 @@ def led_start():
             close()
             sys.exit(0)
         for port in led_port_list:
-            gpio.output(port,True)
-            time.sleep(0.1)
             gpio.output(port,False)
+            time.sleep(0.2)
+            gpio.output(port,True)
 def close():
     gpio.cleanup()
     print 'eixt ...........'
