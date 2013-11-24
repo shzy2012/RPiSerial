d370f064086e41dd9acc5404a9ba1bb9b365f470
shzy2012 <shzy2012@gmail.com>
2013-11-24 16:40:50 +0800

    Update Pin9.py

--- Pin9.py
+++ Pin9.py
@@ -1,5 +1,5 @@
 #import
-import RPi.GPIO  as gio
+import RPi.GPIO  as gpio
 import time
 import sys
 
@@ -9,17 +9,16 @@ led_port_list = [9,11]
 
 # define def  -----Begin
 
-def led_ini():
+def led_init():
     gpio.setmode(gpio.BCM)
-    for port led_port_list:
+    for port in led_port_list:
         gpio.setup(port,gpio.OUT,initial=gpio.LOW)
     
 def led_start():
     while True:
-        print 'exit...'
         if raw_input("Please enter \r\n")=='exit':
             sys.exit(0)
-        for port led_port_list:
+        for port in led_port_list:
             gpio.output(port,True)
             time.sleep(0.1)
             gpio.output(port,False)
